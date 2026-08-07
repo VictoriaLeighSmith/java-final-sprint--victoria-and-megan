f24e19a78b7b8d5fb12f67bb56aa059c50364256
VictoriaLeighSmith <victoria.smith1@keyin.com>
2026-08-07 15:05:24 -0230

    Added database folder and sql file

---
+++ database/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	username TEXT UNIQUE NOT NULL,
+	password TEXT NOT NULL,
+	email TEXT UNIQUE NOT NULL,
+	phone_number TEXT NOT NULL,
+	address TEXT NOT NULL,
+	role TEXT NOT NULL
+);
