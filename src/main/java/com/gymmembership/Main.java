5662477b7b945851c9513110510323576c83efcb
VictoriaLeighSmith <victoria.smith1@keyin.com>
2026-08-07 13:56:26 -0230

    Configured database connection to use properties file

--- src/main/java/com/gymmembership/Main.java
+++ src/main/java/com/gymmembership/Main.java
@@ -1,7 +1,21 @@
 package com.gymmembership;
 
+import com.gymmembership.database.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        // Database connection test
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            System.out.println("Database connection successful!");
+            connection.close();
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
     }
 }
