dc2219c6d4171209c4b42eac85553c643e9e1586
VictoriaLeighSmith <victoria.smith1@keyin.com>
2026-08-09 17:05:50 -0230

    First iteration of user auth

--- src/main/java/com/gymmembership/user/User.java
+++ src/main/java/com/gymmembership/user/User.java
@@ -1,4 +1,106 @@
 package com.gymmembership.user;
 
 public class User {
+
+    // Declare instance variables
+    private int userID;
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String role;
+
+    // Constructors
+    public User(int userID, String username, String password, String email, String phoneNumber, String address, String role) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.role = role;
+    }
+
+    public User(String username, String password, String email, String phoneNumber, String address, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    // Getter and setter methods
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // Override toString method
+    @Override
+    public String toString() {
+        return "User{" +
+                "userID=" + userID +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", address='" + address + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/gymmembership/user/UserDAO.java
+++ src/main/java/com/gymmembership/user/UserDAO.java
@@ -1,4 +1,67 @@
 package com.gymmembership.user;
+import com.gymmembership.database.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDAO {
+
+    public void saveNewUserToDatabase(User user) throws SQLException {
+        // SQL query to add new user to the database
+        String query = "INSERT INTO users (username, password, email, phone_number, address, role) VALUES (?, ?, ?, ?, ?, ?)";
+
+        // Try with resources to set up connection and set prepared statement parameters
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement userStatement = connection.prepareStatement(query)) {
+
+            userStatement.setString(1, user.getUsername());
+            userStatement.setString(2, user.getPassword());
+            userStatement.setString(3, user.getEmail());
+            userStatement.setString(4, user.getPhoneNumber());
+            userStatement.setString(5, user.getAddress());
+            userStatement.setString(6, user.getRole());
+
+            userStatement.executeUpdate();
+        }
+    }
+
+    public User getByUsername(String username) throws SQLException {
+        // SQL query to select user by username from table
+        String query = "SELECT * FROM users WHERE username = ?";
+
+        // Try with resources to set up connection and search for user by username with above query
+        try (Connection connection = DatabaseConnection.getConnection();
+        PreparedStatement userStatement = connection.prepareStatement(query)) {
+
+            userStatement.setString(1, username);
+
+            try (ResultSet resultSet = userStatement.executeQuery()) {
+                // If we get a result back, create a new user object with the results
+                if (resultSet.next()) {
+                    return buildNewUserObject(resultSet);
+                }
+            }
+        }
+        return null;
+    }
+
+    // Separated this method to build a user object from the result set so we can also use it with get all users later
+    private User buildNewUserObject(ResultSet resultSet) throws SQLException {
+        // Create new user object
+        User user  = new User();
+
+        // Use user objects setter methods to set fields
+        user.setUserID(resultSet.getInt("id"));
+        user.setUsername(resultSet.getString("username"));
+        user.setPassword(resultSet.getString("password"));
+        user.setEmail(resultSet.getString("email"));
+        user.setPhoneNumber(resultSet.getString("phone_number"));
+        user.setAddress(resultSet.getString("address"));
+        user.setRole(resultSet.getString("role"));
+
+        // Return user object so we can use it elsewhere
+        return user;
+    }
 }
