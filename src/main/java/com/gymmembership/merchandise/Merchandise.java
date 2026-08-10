51464e30a6155a4f45e7ba30d790bae432b54213
VictoriaLeighSmith <victoria.smith1@keyin.com>
2026-08-10 15:44:53 -0230

    Added merchandise entity class, DAO and service class

--- src/main/java/com/gymmembership/merchandise/Merchandise.java
+++ src/main/java/com/gymmembership/merchandise/Merchandise.java
@@ -1,4 +1,83 @@
 package com.gymmembership.merchandise;
 
 public class Merchandise {
+
+    // Declare variables
+    private int merchandiseID;
+    private String productName;
+    private String type;
+    private double price;
+    private int stockLevel;
+
+    // Constructors
+    public Merchandise(int merchandiseID, String productName, String type, double price, int stockLevel) {
+        this.merchandiseID = merchandiseID;
+        this.productName = productName;
+        this.type = type;
+        this.price = price;
+        this.stockLevel = stockLevel;
+    }
+
+    public Merchandise(String productName, String type, double price, int stockLevel) {
+        this.productName = productName;
+        this.type = type;
+        this.price = price;
+        this.stockLevel = stockLevel;
+    }
+
+    public Merchandise() {
+    }
+
+    // Getter and setter methods
+    public int getMerchandiseID() {
+        return merchandiseID;
+    }
+
+    public void setMerchandiseID(int merchandiseID) {
+        this.merchandiseID = merchandiseID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockLevel() {
+        return stockLevel;
+    }
+
+    public void setStockLevel(int stockLevel) {
+        this.stockLevel = stockLevel;
+    }
+
+    // Override toString method
+    @Override
+    public String toString() {
+        return "Merchandise{" +
+                "merchandiseID=" + merchandiseID +
+                ", productName='" + productName + '\'' +
+                ", type='" + type + '\'' +
+                ", price=" + price +
+                ", stockLevel=" + stockLevel +
+                '}';
+    }
 }
--- src/main/java/com/gymmembership/merchandise/MerchandiseService.java
+++ src/main/java/com/gymmembership/merchandise/MerchandiseService.java
@@ -1,4 +1,102 @@
 package com.gymmembership.merchandise;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class MerchandiseService {
+
+    private final MerchandiseDAO merchandiseDAO = new MerchandiseDAO();
+
+    // Method to save a new product
+    public void addMerchandise(Merchandise merchandise) throws SQLException {
+
+        // Validate that user provided all necessary info
+        if (merchandise.getProductName() == null || merchandise.getProductName().isBlank()) {
+            throw new IllegalArgumentException("Product name must be provided");
+        }
+
+        if (merchandise.getType() == null || merchandise.getType().isBlank()) {
+            throw new IllegalArgumentException("Product type must be provided");
+        }
+
+        if (merchandise.getPrice() <= 0) {
+            throw new IllegalArgumentException("Price must be greater than 0");
+        }
+
+        if (merchandise.getStockLevel() < 0) {
+            throw new IllegalArgumentException("Stock level can't be a negative number");
+        }
+
+        merchandiseDAO.saveNewMerchandiseToDatabase(merchandise);
+    }
+
+    // Method to change product price
+    public void changeProductPrice(int merchandiseID, double newPrice) throws SQLException {
+
+        Merchandise merchandise = merchandiseDAO.getMerchandiseByID(merchandiseID);
+
+        // Validate that user input is valid
+        if (merchandise == null) {
+            throw new IllegalArgumentException("Merchandise with ID " + merchandiseID + " does not exist");
+        }
+
+        if (newPrice <= 0) {
+            throw new IllegalArgumentException("Price must be greater than 0");
+        }
+
+        boolean updatedMerchandise = merchandiseDAO.updateProductPrice(merchandiseID, newPrice);
+
+        if (!updatedMerchandise) {
+            throw new IllegalStateException("Failed to update merchandise with ID " + merchandiseID);
+        }
+    }
+
+    // Method to restock merchandise - we aren't required to remove stock as per requirements but we can add that later if we need
+    public void addStock(int merchandiseID, int quantity) throws SQLException {
+
+        Merchandise merchandise = merchandiseDAO.getMerchandiseByID(merchandiseID);
+
+        // Validate that user input is valid
+        if (merchandise == null) {
+            throw new IllegalArgumentException("Merchandise with ID " + merchandiseID + " does not exist");
+        }
+
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity to add must be greater than 0");
+        }
+
+        // Calculate new stock quantity
+        int newQuantity = merchandise.getStockLevel() + quantity;
+
+        boolean updatedMerchandise = merchandiseDAO.updateProductStockLevel(merchandiseID, newQuantity);
+
+        if (!updatedMerchandise) {
+            throw new IllegalStateException("Failed to update merchandise with ID " + merchandiseID);
+        }
+    }
+
+    // Method to calculate the total value of all inventory
+    public double calculateInventoryValue() throws SQLException {
+
+        // Get all merchandise
+        ArrayList<Merchandise> allMerchandise = merchandiseDAO.getAllMerchandise();
+
+        // Variable to store total value of all merchandise
+        double totalValue = 0;
+
+        // Loop through Array List of merchandise, calculate total value for each item, and add that to total price
+        for (Merchandise merchandise : allMerchandise) {
+            double totalItemPrice = merchandise.getStockLevel() * merchandise.getPrice();
+            totalValue += totalItemPrice;
+        }
+
+        return totalValue;
+    }
+
+    // Method to get all merchandise
+    public ArrayList<Merchandise> browseMerchandise() throws SQLException {
+        return merchandiseDAO.getAllMerchandise();
+    }
 }
+
+
